c7e7e07bfc086d76df8443d1a9bfdf8e781b1fb6
neinlee <139494296+neinlee@users.noreply.github.com>
2024-12-07 18:15:24 +0000

    Update BIRT.js

--- BIRT.js
+++ BIRT.js
@@ -13,6 +13,7 @@ const { round } = util;
 let currentLoop = null;  // 초기 값으로 null을 설정
 let frameDur;  // 전역 변수로 선언
 let introClock;
+let intro_text;
 
 // store info about the experiment session:
 let expName = 'BIRT';  // from the Builder filename that created this script
