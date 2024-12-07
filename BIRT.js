46dbb28a4378c0ef444f2deee90ee150ad37315e
neinlee <139494296+neinlee@users.noreply.github.com>
2024-12-07 18:04:24 +0000

    Update BIRT.js

--- BIRT.js
+++ BIRT.js
@@ -10,6 +10,7 @@ const { Scheduler } = util;
 const { abs, sin, cos, PI: pi, sqrt } = Math;
 const { round } = util;
 
+let currentLoop = null;  // 초기 값으로 null을 설정
 
 // store info about the experiment session:
 let expName = 'BIRT';  // from the Builder filename that created this script
